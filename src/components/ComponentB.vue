0d2947f4690f4961daabfb6a403b6e60825f7795
Khunaiko <pasuaiko21089@gmail.com>
2023-12-16 11:52:11 +0700

    first commit

---
+++ src/components/ComponentB.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+        <p>{{ employee }}</p>
+        <component-c />
+    </div>
+</template>
+
+<script>
+import ComponentC from './ComponentC.vue';
+    export default {
+        name: 'ComponentB',
+        inject:['employee'],
+        components: {
+            ComponentC,
+        
+        },
+        data() {
+            return {
+                name: "ComponentB"
+            }
+        },
+    }
+</script>
